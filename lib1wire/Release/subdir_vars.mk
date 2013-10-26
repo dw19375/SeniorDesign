17b31c304a03700993724fe9acf7f271917fe032
Dan Whisman <whisman1@illinois.edu>
2013-10-25 20:19:02 -0500

    Added Release configuration.

---
+++ lib1wire/Release/subdir_vars.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../onewire.c 
+
+OBJS += \
+./onewire.obj 
+
+C_DEPS += \
+./onewire.pp 
+
+C_DEPS__QUOTED += \
+"onewire.pp" 
+
+OBJS__QUOTED += \
+"onewire.obj" 
+
+C_SRCS__QUOTED += \
+"../onewire.c" 
+
+
---
+++ liblcd/Release/subdir_vars.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../lcd20.c 
+
+OBJS += \
+./lcd20.obj 
+
+C_DEPS += \
+./lcd20.pp 
+
+C_DEPS__QUOTED += \
+"lcd20.pp" 
+
+OBJS__QUOTED += \
+"lcd20.obj" 
+
+C_SRCS__QUOTED += \
+"../lcd20.c" 
+
+
