ffe0f6b07ffd30ded26f1bc049b2cddb32010b25
Dan Whisman <whisman1@illinois.edu>
2013-10-30 21:07:56 -0500

    Implemented functions to send and receive XBee frames.
    Has not been tested.

--- libxbee/spi.c
+++ libxbee/spi.c
@@ -16,77 +16,98 @@
 
 // First two bytes are length,
 // length = 0: already received packet
-static uint8_t rx_buf[MAX_BUF_LEN] = {0, 0};
+extern uint8_t rx_buf[MAX_BUF_LEN];
+extern uint8_t rx_data_len;				// Length of Rx data in rx_buf
+extern uint8_t have_rx_data;
+
+// Pointer to buffer to send
+const uint8_t* to_send;
+
+/*
+ * Initialization of SPI
+ */
+void spi_init()
+{
+	P1SEL |= BIT1 + BIT2 + BIT4;
+	P1SEL2 |= BIT1 + BIT2 + BIT4;
+	UCA0CTL0 |= UCCKPL + UCMSB + UCMST + UCSYNC;  // 3-pin, 8-bit SPI master
+	UCA0CTL1 |= UCSSEL_2;                     // SMCLK
+	UCA0BR0 |= 0x02;                          // /2
+	UCA0BR1 = 0;                              //
+	UCA0MCTL = 0;                             // No modulation
+	UCA0CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
+	IE2 |= UCA0RXIE;                          // Enable USCI0 RX interrupt
+}
 
 
 /*
  * Receives a frame one byte at a time, from UCA0RXBUF.
- * Will write to global buffer incrementally whole frame received,
- * but writes length when whole frame received.
+ * Will write to global buffer incrementally data part of frame and checksum
  * This is intended to be called from an Rx interrupt.
+ *
+ * If there is more data than fits in the buffer, will ignore all bytes
+ * afterward.
  *
  * Assuming UCA0RXBUF contains valid data
  */
-void recv_frame( )
+void spi_recv_frame()
 {
 	static uint16_t len = 0;			// Length of whole frame
-	static uint8_t torx = 0;			// Number of bytes to copy into buffer
 	static uint8_t numbytes = 0;		// Number of bytes received thus far
 
-	// If numbytes == 0 or 1, haven't received whole length yet
-	if( numbytes == 0 )
+	switch( numbytes )
 	{
-		len = 0;
-		len |= UCA0RXBUF << 8;
+		case 0:
+			// First byte received should be 0x7E, if not, silently ignore
+			if( UCA0RXBUF != 0x7E )
+				return;
 
-		// Don't copy to buffer yet, but indicate that it is invalid
-		rx_buf[0] = 0;
-		rx_buf[1] = 0;
-	}
-	// Have received whole length
-	else if( numbytes == 1 )
-	{
-		len |= UCA0RXBUF;
+			len = 0;
+			rx_data_len = 0;
 
-		if( len > MAX_BUF_LEN )
-			torx = MAX_BUF_LEN;
-		else
-			torx = len;
-	}
-	else if( numbytes < (torx-1) )
-	{
-		rx_buf[numbytes] = UCA0RXBUF;
-	}
-	// Receiving last byte now
-	// when numbytes == min(MAX_BUF_LEN-1, len-1)
-	else if( numbytes == torx )
-	{
-		// Copy len over
-		rx_buf[0] = (len >> 8) & 0xFF;
-		rx_buf[1] = len & 0xFF;
-		rx_buf[numbytes] = UCA0RXBUF;
+			break;
+		case 1:
+			// Received MSB of length
+			len = ((uint16_t)UCA0RXBUF) << 8;
+			break;
+		case 2:
+			// Received LSB of length
+			len |= UCA0RXBUF;
+			break;
+		default:
+			// Copy into Rx buffer, if there is room
+			if( rx_data_len < MAX_BUF_LEN )
+			{
+				rx_buf[rx_data_len] = UCA0RXBUF;
+				rx_data_len++;
+			}
+			break;
 	}
 
-	// Must wait for all bytes if len > MAX_BUF_LEN.  Just ignore them.
-
-	numbytes++;
-
-	if( numbytes == len )
+	/*
+	 * If we have received all bytes (numbytes = total-1 -- haven't accounted
+	 * for byte just received), reset numbytes to zero and write to checksum.
+	 */
+	if( numbytes >= len+3 ) // len >= 0, will set after numbytes = 2.
+	{
 		numbytes = 0;
+	}
+	else
+		numbytes++;
 }
 
 /*
  * Returns pointer to received frame if unread, 0x00 if not
  */
-uint8_t* get_frame()
+uint8_t* spi_get_frame()
 {
 	uint8_t* ret = 0x00;
 
-	if( rx_buf[0] || rx_buf[1])
+	if( have_rx_data )
+	{
 		ret = rx_buf;
-
-	rx_buf[0] = 0;
-	rx_buf[1] = 0;
+		have_rx_data = 0;
+	}
 
 	return ret;
 }
@@ -97,30 +118,65 @@ uint8_t* get_frame()
  * for each byte sent.  Length of buffer is int from first two bytes
  * of buf.
  *
- * Assuming it is OK to send right now
+ * Length of packet is in to_send[1] and to_send[2],
+ * Don't make length more than 251 bytes.
  */
-void send_frame( uint8_t* buf )
+void spi_transmit_frame()
 {
 	static uint8_t totx = 0;			// Number of bytes to send total
 	static uint8_t numbytes = 0;		// Number of bytes sent / index of next byte to send
 
 	if( numbytes == 0 )
 	{
-		totx |= buf[0];
-	}
-	else if( numbytes == 1 )
-	{
-		totx |= (buf[1])<<8;
+		totx = to_send[2] + 4;			// LSB of length in XBee data frame
 	}
 
 	if( numbytes == totx )
 	{
-		totx = 0;
 		numbytes = 0;
 	}
 	else
 	{
-		UCA0TXBUF = buf[numbytes];
+		while (!(IFG2 & UCA0TXIFG));              // USCI_A0 TX buffer ready?
+		UCA0TXBUF = to_send[numbytes];
 		numbytes++;
+	}
+}
+
+/*
+ * Send an SPI frame pointed to by buf
+ */
+void spi_send_frame( const uint8_t* buf )
+{
+	// Keep local copy of pointer
+	to_send = buf;
+
+	// Transmit first frame
+	spi_transmit_frame();
+}
+
+/*
+ * Interrupt handler for SPI receive
+ */
+#pragma vector=USCIAB0RX_VECTOR
+__interrupt void USCIA0RX_ISR(void)
+{
+  if( IFG2 & UCA0RXIFG );
+  {
+	  // Receive next byte of frame
+	  spi_recv_frame();
+  }
+}
+
+/*
+ * Interrupt handler for SPI transmit
+ */
+#pragma vector=USCIAB0TX_VECTOR
+__interrupt void USCIA0TX_ISR(void)
+{
+	if( IFG2 & UCA0TXIFG );
+	{
+		// Receive next byte of frame
+		spi_transmit_frame();
 	}
 }
--- libxbee/spi.h
+++ libxbee/spi.h
@@ -18,7 +18,20 @@
 #define IPADDR C0A80180	// 192.168.1.128
 #endif
 
-#define MAX_BUF_LEN 18	// size of Rx buffer to allocate per frame (in bytes)
+/*
+ * XBee IPv4 Rx frame has
+ * 11 bytes header + data (max 8 bytes) + 1 byte checksum = 20 bytes
+ * Leave 1 extra byte for good measure.
+ */
+#define MAX_BUF_LEN 21	// size of Rx buffer to allocate per frame (in bytes)
 
+
+
+/*
+ * Global Variables
+ */
+uint8_t rx_buf[MAX_BUF_LEN] = {0, 0};
+uint8_t rx_data_len = 0;				// Length of Rx data in rx_buf
+uint8_t have_rx_data = 0;
 
 #endif /* SPI_H_ */
